35806d4336225206ec16d07bee1b5cacc164741d
Richik Basu <richikbasuofficial@gmail.com>
2026-09-23 00:39:07 +0530

    updated templates & configurations

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
+    "default": dj_database_url.parse(
+        config("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=not DEBUG,
     )
@@ -180,13 +181,15 @@ REST_FRAMEWORK = {
 }
 
 
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
 SECURE_CONTENT_TYPE_NOSNIFF = True
